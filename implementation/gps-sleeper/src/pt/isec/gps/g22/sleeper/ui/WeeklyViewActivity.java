e76bdbdbb1441faa7d329a74c6fc6fbbefb1df61
Marcos Góis <marcos.avbg@gmail.com>
2014-11-30 16:57:35 +0000

    Added the AndroidPlot library Created the WeeklyView activity

---
+++ implementation/gps-sleeper/src/pt/isec/gps/g22/sleeper/ui/WeeklyViewActivity.java
@@ -0,0 +1,17 @@
+package pt.isec.gps.g22.sleeper.ui;
+
+import pt.isec.gps.a22.sleeper.R;
+import pt.isec.gps.a22.sleeper.R.layout;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class WeeklyViewActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_weekly_view);
+	}
+}

697718f1ecb663c04d305d75177508abacd5a324
Mário Silva <mjgs.biz@gmail.com>
2014-12-10 01:11:14 +0000

    #10: First Implementation

--- implementation/gps-sleeper/src/pt/isec/gps/g22/sleeper/core/TimeUtils.java
+++ implementation/gps-sleeper/src/pt/isec/gps/g22/sleeper/core/TimeUtils.java
@@ -1,5 +1,7 @@
 package pt.isec.gps.g22.sleeper.core;
 
+import java.util.Calendar;
+
 /**
  * Time utility methods
  */
@@ -19,4 +21,40 @@ public class TimeUtils {
 		
 		return (now - dateOfBirth) / (365 * 24 * 60 * 60);
 	}
+	
+    
+    public static String getDate(long unixtime) {
+    	Calendar cal = Calendar.getInstance();
+    	cal.setTimeInMillis(unixtime);
+    	int month = cal.get(Calendar.MONTH)+1;
+    	String s = cal.get(Calendar.YEAR) + "/" + month + "/" + cal.get(Calendar.DAY_OF_MONTH);
+    	return s;
+    }
+    
+    public static String getTime(int time) {
+    	int hours = minutesToHours(time);
+    	int minutes = minutesToMinutes(time);
+    	StringBuilder sb = new StringBuilder();
+    	
+    	if(hours < 10)
+    		sb.append("0");
+    	sb.append(hours);
+    	sb.append(":");
+    	if(minutes < 10)
+    		sb.append("0");
+    	sb.append(minutes);
+    	return sb.toString();
+    }
+    
+    public static int minutesToHours(int time) {
+    	return time / 60;
+    }
+    
+    public static int minutesToMinutes(int time) {
+    	return time % 60;
+    }
+    
+    public static int convertToMinutes(int hours, int minutes) {
+    	return hours * 60 + minutes;
+    }
 }
---
+++ implementation/gps-sleeper/src/pt/isec/gps/g22/sleeper/ui/DayView.java
@@ -0,0 +1,59 @@
+package pt.isec.gps.g22.sleeper.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pt.isec.gps.g22.sleeper.core.SleeperApp;
+import pt.isec.gps.g22.sleeper.core.TimeUtils;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class DayView extends Activity {
+
+	int day;
+	TextView tvDay,tvInsert;
+	ListView dayViewList;
+	CustomAdapter customAdapter;
+	SleeperApp sleeper;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_day_view);
+		
+		sleeper = (SleeperApp)getApplication();
+		
+		Intent intent = getIntent();		
+		day = intent.getIntExtra("day", -1);
+		
+		tvDay = (TextView) findViewById(R.id.tvDay);
+	    dayViewList = (ListView) findViewById(R.id.DayViewList);
+	    tvDay.setText(TimeUtils.getDate(day));
+	    tvInsert = (TextView) findViewById(R.id.tvInsert);
+
+	    setInfo();
+	    
+	    tvInsert.setOnClickListener(new OnClickListener() {
+	    	@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(DayView.this,DailyRecordActivity.class);
+				intent.putExtra("day", day);
+				startActivity(intent);
+			}
+	    });
+	}
+	
+	private void setInfo() {
+	      CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(),sleeper,sleeper.getDayRecordDAO().getRecords(day, day+86400));
+	      dayViewList.setAdapter(customAdapter);
+	}
+	
+}

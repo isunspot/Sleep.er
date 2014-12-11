73cdf797d1a2eca032341d82cb572dcad82aca03
Krupel <gncserrano@gmail.com>
2014-12-11 01:33:07 +0000

    DayView UI

--- implementation/gps-sleeper/src/pt/isec/gps/g22/sleeper/core/TimeUtils.java
+++ implementation/gps-sleeper/src/pt/isec/gps/g22/sleeper/core/TimeUtils.java
@@ -13,48 +13,50 @@ public class TimeUtils {
 	public static int duration(final int hours, final int minutes) {
 		return hours * 60 + minutes;
 	}
-	
+
 	public static long ageFromDateOfBirth(final long dateOfBirth, final long now) {
 		if (dateOfBirth > now) {
-			throw new IllegalArgumentException("The date of birth cannot be in the future");
+			throw new IllegalArgumentException(
+					"The date of birth cannot be in the future");
 		}
-		
+
 		return (now - dateOfBirth) / (365 * 24 * 60 * 60);
 	}
-	
-    
-    public static String getDate(long unixtime) {
-    	Calendar cal = Calendar.getInstance();
-    	cal.setTimeInMillis(unixtime);
-    	int month = cal.get(Calendar.MONTH)+1;
-    	String s = cal.get(Calendar.YEAR) + "/" + month + "/" + cal.get(Calendar.DAY_OF_MONTH);
-    	return s;
-    }
-    
-    public static String getTime(int time) {
-    	int hours = minutesToHours(time);
-    	int minutes = minutesToMinutes(time);
-    	StringBuilder sb = new StringBuilder();
-    	
-    	if(hours < 10)
-    		sb.append("0");
-    	sb.append(hours);
-    	sb.append(":");
-    	if(minutes < 10)
-    		sb.append("0");
-    	sb.append(minutes);
-    	return sb.toString();
-    }
-    
-    public static int minutesToHours(int time) {
-    	return time / 60;
-    }
-    
-    public static int minutesToMinutes(int time) {
-    	return time % 60;
-    }
-    
-    public static int convertToMinutes(int hours, int minutes) {
-    	return hours * 60 + minutes;
-    }
+
+	public static String getDate(long unixtime) {
+		Calendar cal = Calendar.getInstance();
+		unixtime = unixtime * 1000;
+		cal.setTimeInMillis(unixtime);
+		int month = cal.get(Calendar.MONTH) + 1;
+		String s = cal.get(Calendar.YEAR) + "/" + month + "/"
+				+ cal.get(Calendar.DAY_OF_MONTH);
+		return s;
+	}
+
+	public static String getTime(int time) {
+		int hours = minutesToHours(time);
+		int minutes = minutesToMinutes(time);
+		StringBuilder sb = new StringBuilder();
+
+		if (hours < 10)
+			sb.append("0");
+		sb.append(hours);
+		sb.append(":");
+		if (minutes < 10)
+			sb.append("0");
+		sb.append(minutes);
+		return sb.toString();
+	}
+
+	public static int minutesToHours(int time) {
+		return time / 60;
+	}
+
+	public static int minutesToMinutes(int time) {
+		return time % 60;
+	}
+
+	public static int convertToMinutes(int hours, int minutes) {
+		return hours * 60 + minutes;
+	}
 }
